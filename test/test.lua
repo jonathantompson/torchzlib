7e202bd24402ab8776d1c3290c30ce04845e997e
Jonathan Tompson <jonathantompson@gmail.com>
2015-04-13 16:53:46 -0400

    removed min and max prints in test.lua.

--- test/test.lua
+++ test/test.lua
@@ -6,7 +6,7 @@ for _, cur_type in pairs(test_types) do
   for apply_sub_filter = 0, 1 do
     for quality = 0, 2 do
 
-      local data = torch.rand(125,126):mul(255):clamp(0,255):type(cur_type)
+      local data = torch.rand(4,1,125,126):mul(255):clamp(0,255):type(cur_type)
       local data_compressed = torch.CompressedTensor(data, quality, apply_sub_filter == 1)
 
       -- Make sure data integrety is maintained when serializing to / from disk
@@ -21,8 +21,6 @@ for _, cur_type in pairs(test_types) do
         assert(err:max() == 0 and err:min() == 0,  -- Lossless!  It should be exactly the same
           'failed on '..cur_type..', quality='..quality..', apply_sub_filter='..apply_sub_filter)
       else
-        print(err:max())
-        print(err:min())
         assert(err:max() < 1e-3 and err:min() > -1e-3,  -- Filter will introduce floating point error
           'failed on '..cur_type..', quality='..quality..', apply_sub_filter='..apply_sub_filter)
       end
